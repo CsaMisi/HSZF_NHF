f09b03c7fc8948a228f6a164a7452df1b125f850
buffMidget <misicsanko@gmail.com>
2025-03-28 21:53:59 +0100

    refactored for better separations between layers

--- NVRTMG_HSZF_2024251/NVRTMG_HSZF_2024251.Model/obj/Debug/net8.0/NVRTMG_HSZF_2024251.Model.AssemblyInfo.cs
+++ NVRTMG_HSZF_2024251/NVRTMG_HSZF_2024251.Model/obj/Debug/net8.0/NVRTMG_HSZF_2024251.Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NVRTMG_HSZF_2024251.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1478c3110eb5a598ef234b607338ada11d38a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NVRTMG_HSZF_2024251.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NVRTMG_HSZF_2024251.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
